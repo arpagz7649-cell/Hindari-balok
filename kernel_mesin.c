96c66ba22b9eb38b189268b61dafbd559bf5824c
arpagz7649-cell <arpagz7649@gmail.com>
2026-03-03 11:59:38 +0700

    Create kernel_mesin.c

---
+++ kernel_mesin.c
@@ -0,0 +1,14 @@
+/* --- Ini adalah kode Bahasa C --- */
+#include <stdio.h>
+
+int main() {
+    // 1. Variabel dengan tipe data yang ketat
+    int skor = 100;
+    char grade = 'A';
+
+    // 2. Menampilkan output ke layar
+    printf("Selamat, Master!\n");
+    printf("Skor anda adalah: %d dengan Grade: %c\n", skor, grade);
+
+    return 0;
+}
